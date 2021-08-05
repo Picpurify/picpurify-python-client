32c8d04748cb8c778732e26d4202eae0f525e6ed
Félix Jamet <felix.jamet@arsnovasystems.com>
2021-08-05 12:34:36 +0200

    Minor reorganisation and documentation

--- picpurify/api.py
+++ picpurify/api.py
@@ -21,22 +21,47 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-
 '''
 
 import os, json, requests
 import picpurify
 
+#########
+# Utils #
+#########
+class PicpurifyException(Exception):
+    def __init__(self, errorCode, errorMsg):
+        self.errorCode = errorCode
+        self.errorMsg = errorMsg
 
+    def __str__(self):
+        return 'Error code ' + str(self.errorCode) + ': ' + self.errorMsg
+
+def getNbFace(api_key, image_path):
+    client = PicPurifyImage(api_key, ['face_detection'])
+    response = client.analyse(image_path)
+    return response["face_detection"]["nb_face"]
+
+
+##########################
+# Actual PicPurify logic #
+##########################
 class PicPurify(object):
     '''
-    
+    Base class of the analyser, should not be used directly.
+    Its user-facing derived classes are PicPurifyImage and PicPurifyVideo.
     '''
-    MODERATION_TASKS = ['porn_moderation', 'suggestive_nudity_moderation', 'gore_moderation', 'qr_code_moderation', 'money_moderation',
-                        'weapon_moderation', 'drug_moderation', 'hate_sign_moderation', 'obscene_gesture_moderation','qr_code_moderation']
-    DETECTION_TASKS = ['face_detection', 'face_gender_detection', 'face_age_detection', 'face_gender_age_detection']
+    MODERATION_TASKS = [
+        'porn_moderation', 'suggestive_nudity_moderation', 'gore_moderation',
+        'qr_code_moderation', 'money_moderation', 'weapon_moderation',
+        'drug_moderation', 'hate_sign_moderation', 'obscene_gesture_moderation',
+        'qr_code_moderation'
+    ]
+    DETECTION_TASKS = [
+        'face_detection', 'face_gender_detection', 'face_age_detection',
+        'face_gender_age_detection'
+    ]
     AVAILABLE_TASKS = MODERATION_TASKS + DETECTION_TASKS + ['content_moderation_profile']
-    
 
     def __init__(self, api_key):
         if type(self) is PicPurify:
@@ -44,8 +69,8 @@ class PicPurify(object):
         self.API_KEY = api_key
         self.session = requests.Session()
         self.session.headers = requests.utils.default_headers()
-        self.session.headers.update({'User-Agent': 'PicPurify-Client-Python-' + picpurify.__version__ })
-        
+        self.session.headers.update({'User-Agent': 'PicPurify-Client-Python-' + picpurify.__version__})
+
     def updateTasks(self, tasks):
         if type(tasks) is not list:
             raise ValueError('The task parameter must be a list of available task '+ str(tasks) +'\nAvailable tasks :\n' + '\n'.join(self.AVAILABLE_TASKS))
@@ -54,8 +79,6 @@ class PicPurify(object):
                 raise ValueError('This task is not available '+ str(task)+ '\nPlease check the task name or upgrade your package if this task is not available in this package version.')            
         self.tasks = tasks
 
-        
-    
     def isSafe(self, data_path, **optional_parameters):
         #check if tasks is only focusing on detection 
         if len(self.tasks) == len(set(self.tasks) & set(self.DETECTION_TASKS)):
@@ -64,28 +87,25 @@ class PicPurify(object):
         if "final_decision" in response:
             return response["final_decision"] == "OK"
 
-       
     def analyseFile(self, file_path, **optional_parameters):
         file_data = {self.file_field: open(file_path, 'rb')}
         post_data =  {"API_KEY": self.API_KEY, "task": ','.join(self.tasks)}
         post_data.update(optional_parameters)
         result_data = requests.post(self.endpoint, files = file_data, data = post_data, headers = self.session.headers)
         return json.loads(result_data.content)
-    
-    
+
     def analyseUrl(self, url_data, **optional_parameters):
         post_data =  { self.url_field : url_data, "API_KEY":self.API_KEY, "task": ','.join(self.tasks)}
         post_data.update(optional_parameters)
         result_data = requests.post(self.endpoint, data = post_data, headers = self.session.headers)
         return json.loads(result_data.content)
-    
-    
+
     def analyse(self, data_path, **optional_parameters):
         if data_path.lower().startswith(("http://", "https://","data:image/")):
             response = self.analyseUrl(data_path, **optional_parameters)
         else:
             response = self.analyseFile(data_path, **optional_parameters)
-        
+
         if "status" not in response:
             raise  PicpurifyException(50,"Cannot get valid answer from Picpurify endpoint")          
         if response["status"] == "success":
@@ -94,9 +114,6 @@ class PicPurify(object):
             raise PicpurifyException(response["error"]["errorCode"],response["error"]["errorMsg"])
 
 
-
-
-        
 class PicPurifyImage(PicPurify):
     '''
     IMAGE API DOC : https://www.picpurify.com/api-services.html#single_image_api_doc
@@ -105,7 +122,6 @@ class PicPurifyImage(PicPurify):
     reference_id     String    A unique reference associated to the image in your information system
     origin_id        String    A reference to retrieve the origin of the image, profile id, account id ...
     '''
-       
     def __init__(self, api_key, tasks):
         '''
         api_key: your personnal API key (can be found in the "API keys" section in your dashboard
@@ -114,16 +130,15 @@ class PicPurifyImage(PicPurify):
         super(PicPurifyImage, self).__init__(api_key)
         self.updateTasks(tasks)
         self.endpoint = 'https://www.picpurify.com/analyse/1.1'
-        
+
     @property
     def file_field(self):
         return 'file_image'
-    
+
     @property
     def url_field(self):
         return 'url_image'
-                 
-    
+
 
 class PicPurifyVideo(PicPurify):
     '''
@@ -134,8 +149,6 @@ class PicPurifyVideo(PicPurify):
     reference_id     String    A unique reference associated to the video in your information system
     origin_id        String    A reference to retrieve the origin of the video, profile id, account id ...
     '''
-    
-    
     def __init__(self, api_key, tasks):
         '''
         api_key: your personnal API key (can be found in the "API keys" section in your dashboard
@@ -144,27 +157,13 @@ class PicPurifyVideo(PicPurify):
         super(PicPurifyVideo, self).__init__(api_key)
         self.updateTasks(tasks)
         self.endpoint = 'https://www.picpurify.com/analyse_video/1.1'
-         
+
     @property
     def file_field(self):
         return 'file_video'
-    
+
     @property
     def url_field(self):
         return 'url_video'
-        
-
-class PicpurifyException(Exception):
-    def __init__(self, errorCode, errorMsg):
-        self.errorCode = errorCode
-        self.errorMsg = errorMsg
-        
-    def __str__(self):
-        return 'Error code ' + str(self.errorCode) + ' : ' + self.errorMsg
-            
-def getNbFace(api_key, image_path):
-    client = PicPurifyImage(api_key, ['face_detection'])
-    response = client.analyse(image_path)
-    return response["face_detection"]["nb_face"]
 
 
